990c0f1e3a8018234cc507e19fc8f4841c4d328b
sethcarl2000 <sethhall@jlab.org>
2026-04-15 11:54:49 -0400

    Add ArmMode.h struct

---
+++ src/ArmMode.h
@@ -0,0 +1,20 @@
+#ifndef ArmMode_H
+#define ArmMode_H
+
+#include <stdint.h> 
+
+/// @brief Bit flag to indicate which arm to use. 
+namespace ArmMode {
+    enum Bit : int8_t { 
+        kNone=0, 
+        kRHRS=1<<0,
+        kLHRS=1<<1, 
+        kBoth=(kRHRS | kLHRS)
+    };
+
+    //this is to keep compatability with older code which has not made the switch to the bit flag defined above. 
+    constexpr bool kRHRS_bool = true; 
+    constexpr bool kLHRS_bool = false; 
+}
+
+#endif 
