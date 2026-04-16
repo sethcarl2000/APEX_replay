214c3562bcbf8d467ae9accc7f6230280963b2d0
sethcarl2000 <sethhall@jlab.org>
2026-04-16 11:00:27 -0400

    Add snapshot functionality to RDFNodeAccumulator

--- include/RDFNodeAccumulator.h
+++ include/RDFNodeAccumulator.h
@@ -20,10 +20,14 @@ public:
     }; 
 
 private: 
-    std::vector<ROOT::RDF::RNode> fNodes;     
+    ROOT::RDF::RNode fNode;     
     std::ostringstream fErrorMsg; 
     EStatus fStatus{kGood};
     bool fAbortOnError{true}; 
+
+
+    // list of branches to include in an output file. 
+    std::vector<std::string> fOutputBranches; 
 
 public: 
     RDFNodeAccumulator(ROOT::RDF::RNode start); 
@@ -49,7 +53,7 @@ public:
     bool IsBranchDefined(std::string branch); 
 
     //return a count at the current last node
-    ROOT::RDF::RResultPtr<ULong64_t> Count() { return fNodes.back().Count(); }
+    ROOT::RDF::RResultPtr<ULong64_t> Count() { return fNode.Count(); }
 
     //bool IsBranchDefined(const char* branch) const { string str(branch); return IsBranchDefined(str); }
 
@@ -63,7 +67,7 @@ public:
 
     bool IsBranchDefined(const char* branch); 
 
-    ROOT::RDF::RNode& Get() { return fNodes.back(); }
+    ROOT::RDF::RNode& Get() { return fNode; }
 
     EStatus GetStatus() const { return fStatus; }
     std::string GetErrorMsg() const { return fErrorMsg.str(); }
@@ -74,13 +78,21 @@ public:
         std::abort(); 
     }
 
+    //add a pre-existing branch to the output
+    void AddBranchToOutput(std::string branch) { fOutputBranches.push_back(branch); }
+
+    //define a branch specifically for the output. 
+    template<typename F> void DefineOutput(std::string new_branch, F expression, const std::vector<std::string>& inputs);
+    
+    void Snapshot(std::string tree_name, std::string path_outfile); 
+
     //set whether an abort should be called when an error is encountered. 
     void SetAbortOnError(bool _val) { fAbortOnError=_val; }
 
     //assignment operator 
-    ROOT::RDF::RNode& operator=(ROOT::RDF::RNode node) {
-        fNodes.emplace_back(node); 
-        return fNodes.back(); 
+    ROOT::RDF::RNode& operator=(const ROOT::RDF::RNode& node) {
+        fNode = node; 
+        return fNode; 
     }
     ClassDef(RDFNodeAccumulator,1); 
 };
@@ -88,19 +100,17 @@ public:
 //now, we get actual class definitions. 
 
 //__________________________________________________________________________________________________________________________________
-RDFNodeAccumulator::RDFNodeAccumulator(ROOT::RDF::RNode start) : fNodes{ start } {/* noop */}
+RDFNodeAccumulator::RDFNodeAccumulator(ROOT::RDF::RNode start) : fNode{ start } {/* noop */}
 
 //__________________________________________________________________________________________________________________________________
 RDFNodeAccumulator::RDFNodeAccumulator(const char* tree_name, const char* path_infile)
+    : fNode{ROOT::RDataFrame(tree_name, path_infile)}
 {
-    //try to construct RDataFrame
+    /*/try to construct RDataFrame
     try {
 
-        fNodes.clear(); 
-
         ROOT::RDataFrame df(tree_name, path_infile); 
-
-        fNodes.emplace_back(df); 
+        fNode = df;
 
     } catch (const std::exception& e) {
 
@@ -111,7 +121,7 @@ RDFNodeAccumulator::RDFNodeAccumulator(const char* tree_name, const char* path_i
          
         PrintMsgAndAbort(); 
         return; 
-    }
+    }*/ 
 }
 
 //__________________________________________________________________________________________________________________________________
@@ -136,7 +146,7 @@ template<typename F> void RDFNodeAccumulator::Define(const char* new_branch, F e
     }
 
     try {        
-        fNodes.emplace_back( fNodes.back().Define(new_branch, expression, inputs) ); 
+        fNode = fNode.Define(new_branch, expression, inputs); 
     
     } catch (const std::exception& e) {
         //Error("RDFNodeAccumulator::Define", "exception caught while trying to define new branch '%s'.\n -- what(): %s", new_branch, e.what());
@@ -156,9 +166,9 @@ template<typename F> void RDFNodeAccumulator::Overwrite(const char* new_branch, 
 
     try {
         if (IsBranchDefined(new_branch)) { 
-            fNodes.emplace_back( fNodes.back().Redefine(new_branch, expression, inputs) ); 
+            fNode = fNode.Redefine(new_branch, expression, inputs); 
         } else {
-            fNodes.emplace_back( fNodes.back().Define(new_branch, expression, inputs) ); 
+            fNode = fNode.Define(new_branch, expression, inputs); 
         }
         
     } catch (const std::exception& e) {
@@ -177,12 +187,12 @@ template<typename F> void RDFNodeAccumulator::DefineIfMissing(const char* new_br
 //__________________________________________________________________________________________________________________________________
 template<typename F> void RDFNodeAccumulator::Filter(F expression, const std::vector<std::string>& inputs)
 {
-    fNodes.emplace_back ( fNodes.back().Filter(expression, inputs) ); 
+    fNode = fNode.Filter(expression, inputs); 
 } 
 //__________________________________________________________________________________________________________________________________
 bool RDFNodeAccumulator::IsBranchDefined(std::string branch) 
 {
-    for (const std::string& column : fNodes.back().GetColumnNames()) { if (branch == column) return true; }
+    for (const std::string& column : fNode.GetColumnNames()) { if (branch == column) return true; }
     return false; 
 }
 //__________________________________________________________________________________________________________________________________
@@ -211,7 +221,52 @@ bool RDFNodeAccumulator::IsBranchDefined(const char* branch)
     return IsBranchDefined(branch_str); 
 }
 //__________________________________________________________________________________________________________________________________
+template<typename F> void RDFNodeAccumulator::DefineOutput(std::string new_branch, F expression, const std::vector<std::string>& inputs)
+{
+    fOutputBranches.push_back(new_branch); 
+    Define(new_branch, expression, inputs);  
+}
 //__________________________________________________________________________________________________________________________________
+void RDFNodeAccumulator::Snapshot(std::string tree_name, std::string path_outfile)
+{
+    if (fOutputBranches.empty()) {
+        fErrorMsg << "\n - <Snapshot>: no output branches! you may define them using 'DefineOutput' or 'AddBranchToOutput'.\n";
+        fStatus = kError; 
+        if (fAbortOnError) { PrintMsgAndAbort(); }
+        return; 
+    }
+
+    //first, let's scan to be sure that we have all the branches we need. 
+    std::vector<std::string> missing_branches{}; 
+    for (const auto& out_br : fOutputBranches) {
+        if (!IsBranchDefined(out_br.c_str())) missing_branches.push_back(out_br); 
+    }
+
+    if (!missing_branches.empty()) {
+        fErrorMsg << 
+        "\n"
+        "- <Snapshot>: tried to write to output file, but the following branches are missing:\n"; 
+        for (auto& br : missing_branches) fErrorMsg << "               " << br << "\n";
+        
+        fStatus = kError; 
+        if (fAbortOnError) { PrintMsgAndAbort(); }
+        return; 
+    }
+
+    try {
+        fNode.Snapshot(tree_name, path_outfile, fOutputBranches); 
+    } catch (const std::exception& e) {
+        fErrorMsg << 
+        "\n"
+        "- <Snapshot>: something unexpected went wrong trying to make a snapshot.\n" 
+        "              what(): "<< e.what() <<"\n";
+        
+        fStatus = kError; 
+        if (fAbortOnError) { PrintMsgAndAbort(); }
+        return; 
+    }
+
+}
 //__________________________________________________________________________________________________________________________________
 //__________________________________________________________________________________________________________________________________
 //__________________________________________________________________________________________________________________________________
