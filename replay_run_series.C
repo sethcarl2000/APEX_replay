d426441248ca1040cdcda6bce6f32860b8f782bf
sethcarl2000 <sethhall@jlab.org>
2026-04-24 18:29:20 -0400

    Add 'replay_run_series' to allow serial execution of replay to expediate process

---
+++ replay_run_series.C
@@ -0,0 +1,61 @@
+#include <TROOT.h>
+#include <TStopwatch.h>
+#include <TFile.h> 
+#include <TTree.h> 
+#include <TError.h>
+#include <TString.h>
+
+#include "vdc_track_replay.C" 
+
+#include <vector>
+#include <utility> 
+#include <string>
+#include <stdio.h> 
+
+/// this macro is meant to compile the 'vdc_track_replay' macro, and process the
+/// seires of runs which are input. 
+
+//forward-declare this fcn, so the symbol is defined 
+int vdc_track_replay(std::string, std::string, int, std::string, ULong64_t); 
+
+int replay_run_series(
+  const int run_number,
+  std::string stem_output, 
+  const std::vector<std::string>& paths_input, 
+  std::string mode )
+{
+  using namespace std; 
+  
+  printf(
+    "<replay_run_series>: There are %zi files to process ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n",
+    paths_input.size()
+  );
+
+  int i_part=0; 
+  for (const auto& path_input : paths_input) {
+    
+    string path_output = Form("%s.%i.part-%i.root",
+			      stem_output.data(),
+			      run_number,
+			      i_part++ 
+			      );
+
+    printf(
+      "<replay_run_series>: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+      "                     Processing sub-part %i/%zi\n"
+      "                     Input file '%s' ...\n"
+      "                     Output file '%s' ...\n",
+      i_part, paths_input.size(), 
+      path_input.data(),
+      path_output.data() 
+    );
+    
+    vdc_track_replay(path_input, path_output, run_number, mode, 0);
+  }
+
+  printf(
+    "<replay_run_series>: done. ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+  ); 
+  
+  return 0; 
+}
