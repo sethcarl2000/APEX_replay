167a35eb18b1fcfc90d61a8a60ac653f93fafeb1
sethcarl2000 <sethhall@jlab.org>
2026-04-26 23:38:07 -0400

    Add UniqueIDGenerator.h

---
+++ src/UniqueIDGenerator.h
@@ -0,0 +1,32 @@
+#ifndef UniqueIDGenerator_H  
+#define UniqueIDGenerator_H 
+
+#include <atomic>
+#include <EventCounter.h> 
+
+class UniqueIDGenerator { 
+  
+ public: 
+  /*** 
+   *    Generates Unique ID each time it's called in thread-safe manner
+   * 
+   ***/
+
+  UniqueIDGenerator() : fCounter{0} {};
+  virtual ~UniqueIDGenerator() {}; 
+  
+  /// @return ID number which is unique across all threads 
+  EventCounter GetID() { return fCounter.fetch_add(1, std::memory_order_relaxed); }
+
+  //delete copy / move constructors
+  UniqueIDGenerator(const UniqueIDGenerator&) = delete;
+  UniqueIDGenerator& operator=(const UniqueIDGenerator&) = delete;
+  
+private:
+
+  std::atomic<EventCounter> fCounter; 
+  
+  ClassDef(UniqueIDGenerator,1);
+}; 
+
+#endif 
