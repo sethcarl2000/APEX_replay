a52f14ae912bc9fe024cd6ddc11583706730d8c5
sethcarl2000 <sethhall@jlab.org>
2026-05-29 12:11:15 -0400

    Add macro to draw coinc times across all runs

---
+++ macros/draw_coinc_time.C
@@ -0,0 +1,36 @@
+
+#include <TH2D.h>
+#include "AnalyzeAllData.cxx"
+
+int draw_coinc_time()
+{
+
+  //
+  AnalyzeAllData ana(20); 
+  
+  auto hist_coinc = new TH2D("h_coinc", "T_{R} - T_{L} coinc time;run number;coinc time (ns)",
+			     200, 3800,4800,
+			     200, -100,+100);
+
+  hist_coinc->SetDirectory(0); 
+
+  double ns = 1e-9; 
+
+  //__________________________________________________________________________________________
+  std::function<ROOT::RDF::RNode(ROOT::RDF::RNode)> scale_time = [ns](ROOT::RDF::RNode df) {
+    auto df_out = df
+      .Redefine("S2R_S2L_dt_center", [ns](double t) { return t/ns; }, {"S2R_S2L_dt_center"})
+      .Redefine("S2R_S2L_dt_sigma",  [ns](double t) { return t/ns; }, {"S2R_S2L_dt_sigma"});
+
+    return df_out;
+  }; 
+  //__________________________________________________________________________________________
+  
+  ana.Fill_TH2D(hist_coinc, "run_number", "S2R_S2L_dt_center", &scale_time, "meta_data");
+
+  hist_coinc->SetMarkerStyle(kOpenCircle);
+  hist_coinc->SetMarkerSize(0.70); 
+  hist_coinc->Draw();
+
+  return 0; 
+}
