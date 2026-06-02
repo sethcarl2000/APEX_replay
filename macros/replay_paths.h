edaf8669d46451c7d43faa8d92688a63b790e9b7
sethcarl2000 <sethhall@jlab.org>
2026-06-02 14:09:55 -0400

    Add replay_paths as a namespace

---
+++ macros/replay_paths.h
@@ -0,0 +1,61 @@
+#ifndef replay_paths_h
+#define replay_paths_h
+
+#include <vector> 
+#include <string>
+
+namespace replay_paths {
+  //list of all replay files (most recent replay)
+  const std::vector<std::string> list{
+    "/volatile/halla/apex/full_replay/production/replay-3800-3824.root",
+    "/volatile/halla/apex/full_replay/production/replay-3825-3849.root",
+    "/volatile/halla/apex/full_replay/production/replay-3850-3874.root",
+    "/volatile/halla/apex/full_replay/production/replay-3875-3899.root",
+    "/volatile/halla/apex/full_replay/production/replay-3900-3924.root",
+    "/volatile/halla/apex/full_replay/production/replay-3925-3949.root",
+    "/volatile/halla/apex/full_replay/production/replay-3950-3974.root",
+    "/volatile/halla/apex/full_replay/production/replay-3975-3999.root",
+    "/volatile/halla/apex/full_replay/production/replay-4000-4024.root",
+    "/volatile/halla/apex/full_replay/production/replay-4025-4049.root",
+    "/volatile/halla/apex/full_replay/production/replay-4050-4074.root",
+    "/volatile/halla/apex/full_replay/production/replay-4075-4099.root",
+    "/volatile/halla/apex/full_replay/production/replay-4100-4124.root",
+    "/volatile/halla/apex/full_replay/production/replay-4125-4149.root",
+    "/volatile/halla/apex/full_replay/production/replay-4150-4174.root",
+    "/volatile/halla/apex/full_replay/production/replay-4175-4199.root",
+    "/volatile/halla/apex/full_replay/production/replay-4200-4224.root",
+    "/volatile/halla/apex/full_replay/production/replay-4225-4249.root",
+    "/volatile/halla/apex/full_replay/production/replay-4250-4274.root",
+    "/volatile/halla/apex/full_replay/production/replay-4275-4299.root",
+    "/volatile/halla/apex/full_replay/production/replay-4300-4324.root",
+    "/volatile/halla/apex/full_replay/production/replay-4325-4349.root",
+    "/volatile/halla/apex/full_replay/production/replay-4350-4374.root",
+    "/volatile/halla/apex/full_replay/production/replay-4375-4399.root",
+    "/volatile/halla/apex/full_replay/production/replay-4400-4424.root",
+    "/volatile/halla/apex/full_replay/production/replay-4425-4449.root",
+    "/volatile/halla/apex/full_replay/production/replay-4450-4474.root",
+    "/volatile/halla/apex/full_replay/production/replay-4475-4499.root",
+    "/volatile/halla/apex/full_replay/production/replay-4500-4524.root",
+    "/volatile/halla/apex/full_replay/production/replay-4525-4549.root",
+    "/volatile/halla/apex/full_replay/production/replay-4550-4574.root",
+    "/volatile/halla/apex/full_replay/production/replay-4575-4599.root",
+    "/volatile/halla/apex/full_replay/production/replay-4600-4624.root",
+    "/volatile/halla/apex/full_replay/production/replay-4625-4649.root",
+    "/volatile/halla/apex/full_replay/production/replay-4650-4674.root",
+    "/volatile/halla/apex/full_replay/production/replay-4675-4699.root",
+    "/volatile/halla/apex/full_replay/production/replay-4700-4724.root",
+    "/volatile/halla/apex/full_replay/production/replay-4725-4749.root",
+    "/volatile/halla/apex/full_replay/production/replay-4750-4774.root",
+    "/volatile/halla/apex/full_replay/production/replay-4775-4799.root",
+    "/volatile/halla/apex/full_replay/production/replay-4800-4824.root",
+    "/volatile/halla/apex/full_replay/production/replay-4825-4849.root",
+    "/volatile/halla/apex/full_replay/production/replay-4850-4874.root",
+    "/volatile/halla/apex/full_replay/production/replay-4875-4899.root",
+    "/volatile/halla/apex/full_replay/production/replay-4900-4924.root",
+    "/volatile/halla/apex/full_replay/production/replay-4925-4949.root",
+    "/volatile/halla/apex/full_replay/production/replay-4950-4974.root",
+    "/volatile/halla/apex/full_replay/production/replay-4975-4999.root"
+  };
+}; 
+
+#endif
