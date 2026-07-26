1a372594ab399e1a3b201c2326b59c8947a6dc9e
sethcarl2000 <sethhall@jlab.org>
2026-07-26 15:54:01 -0400

    Clean up the env setup script

--- set_apex_replay_env.sh
+++ set_apex_replay_env.sh
@@ -40,48 +40,38 @@ DB_DIR="${PATH_APEX_REPLAY}/analyzer/DB"
 #now, add back in *our* version of the analyzer 
 #export LD_LIBRARY_PATH="${ANALYZER_LD_PATH}:${NEW_LD_LIBRARY_PATH}" 
 
-function find-path-in-list () {
-    while read -d ':' line; do
-	if [[ "${line}" == "${2}" ]]; then
-	    echo "found"
-	    exit 0
-	fi
-    done < <(echo "${1}")
-    echo "not found"
-    exit 0
-}
 
-
-
-LOADED_APEX=$(find-path-in-list "${LD_LIBRARY_PATH}" "${PATH_APEX_REPLAY}/src/build")
-#while read -d ':' line; do
-#    if [[ "${line}" == "${PATH_APEX_REPLAY}/src/build" ]]; then
-#	LOADED_APEX=1 
-#	break; 
-#    fi  
-#done < <(echo "${LD_LIBRARY_PATH}")
-
-if [[ ${LOADED_APEX} == "not found" ]]; then
-    echo "<${0}> apex lib not yet loaded" 
-    export LD_LIBRARY_PATH="${PATH_APEX_REPLAY}/decode/build:${LD_LIBRARY_PATH}"
-else
-    echo "<${0}> apex lib already loaded"
-fi
-
-APEX_IN_INCLUDE_DIR=$(find-path-in-list "${ROOT_INCLUDE_PATH}" "${PATH_APEX_REPLAY}/src")
-
-if [[ ${APEX_IN_INCLUDE_DIR} == "not found" ]]; then
-    echo "<${0}> apex not yet in include path. adding..."
-    export ROOT_INCLUDE_PATH="${ROOT_INCLUDE_PATH}:${PATH_APEX_REPLAY}/src"
-else 
-    echo "<${0}> apex src already in root include path"
-fi
+#APEX_IN_INCLUDE_DIR=$(find-path-in-list "${ROOT_INCLUDE_PATH}" "${PATH_APEX_REPLAY}/src")
+#
+#if [[ ${APEX_IN_INCLUDE_DIR} == "not found" ]]; then
+#    echo "<${0}> apex not yet in include path. adding..."
+#    export ROOT_INCLUDE_PATH="${ROOT_INCLUDE_PATH}:${PATH_APEX_REPLAY}/src"
+#else 
+#    echo "<${0}> apex src already in root include path"
+#fi
     
 # now, let's define some other environment variables:
 export PATH_APEX_CACHE="/cache/halla/apex/raw"
 export PATH_APEX_VOLATILE="/volatile/halla/apex/full_replay" 
 export PATH_APEX_SCRIPTS="${PATH_APEX_REPLAY}/scripts"
 export PATH_APEX_MACROS="${PATH_APEX_REPLAY}/macros" 
+
+# make sure we have this script to add this path to the list
+if [[ ! -x "/home/$(whoami)/.local/bin/add-path-to-list" ]]
+then
+    echo "<${0}>: Error: script 'add-path-to-list' not found in '~/.local/bin'."
+    echo "you can add it by running the following bash command:"
+    echo ""
+    echo "     ln -s \"/home/sethhall/scripts/add-path-to-list\" \"/home/$(whoami)/.local/bin/.\"" 
+    echo "" 
+    exit
+fi
+
+source add-path-to-list "${PATH_APEX_REPLAY}/src" ROOT_INCLUDE_PATH
+
+source add-path-to-list "${PATH_APEX_REPLAY}/src/build" LD_LIBRARY_PATH
+
+exit
 
 # add our macros to the list of macros
 if [ ! -f "~/.rootrc" ]; then
