484335ae7cabcd11538c5b98e104c29089170771
Seth Hall <sethhall@jlab.org>
2026-08-28 10:22:20 -0400

    Fix namespace names

--- include/APEX/EventCounter.h
+++ include/APEX/EventCounter.h
@@ -1,10 +1,15 @@
-#ifndef EventCounter_H
-#define EventCounter_H
+#ifndef APEX_EventCounter_H
+#define APEX_EventCounter_H
 
 #include <ROOT/RResultPtr.hxx> 
 
+namespace APEX
+{
+    
 /// @brief simple rename for a RResultPtr which can be used to count the number of events passing a given analysis step 
 using EventCounter = ULong64_t; 
 using EventCounter_RPtr = ROOT::RDF::RResultPtr<ULong64_t>; 
+
+}
 
 #endif
