32807b706b4c626d942f0263957ec0e210bbea0f
Seth Hall <sethhall@jlab.org>
2026-08-28 09:58:53 -0400

    Add explicit headers for analysis classes

--- include/APEX/replay/helpers.h
+++ include/APEX/replay/helpers.h
@@ -1,20 +1,20 @@
 #ifndef APEX_replay_helpers_h
 #define APEX_replay_helpers_h
 
+// APEX headers
+#include <APEX/EventHandler.h>
+#include <APEX/ReactVertex.h>
+#include <APEX/S2Hit.h>
+#include <APEX/PidManager.h>
+// ---  VDC headers
+#include <APEX/VDC/Track.h>
+#include <APEX/VDC/ChamberPair.h>
+#include <APEX/VDC/HitGroup.h>
+// --- replay header
 #include <APEX/replay.h> 
 #include <RDFNodeAccumulator.h>
 // ROOT headers
 #include <ROOT/RVec.hxx>
-
-class TapexEventHandler; 
-class TapexReactVertex; 
-class TapexS2Hit; 
-class ApexPidManager; 
-namespace ApexVDC { 
-    class Track;
-    class ChamberPair;
-    class HitGroup; 
-}; 
 
 
 namespace APEX
@@ -26,8 +26,8 @@ namespace helpers
 
 
 double grid_search( 
-    const TapexEventHandler& evt, 
-    ApexVDC::HitGroup& group, 
+    const EventHandler& evt, 
+    VDC::HitGroup& group, 
     double m1, //slope 
     double m2, 
     double x_Lo,
@@ -35,50 +35,50 @@ double grid_search(
     double TAU_sigma  =9e-9, 
     double TAU_buffer =20e-9 );
 
-void Compute_trackError(ApexVDC::Track& trk);
+void Compute_trackError(VDC::Track& trk);
 
-void compute_trackdata(ApexVDC::Track &trk); 
+void compute_trackdata(VDC::Track &trk); 
 
-ROOT::RVec<TapexEventHandler> gen_coinc_events(
+ROOT::RVec<EventHandler> gen_coinc_events(
     double dt_center,  //the 'central' TR - TL for this event
     double dt_cut,     //the cut for events too far away from this value
     double beam_current, 
     unsigned int run_number,
-    const ROOT::RVec<TapexS2Hit>& R_s2_hits,
-    const ROOT::RVec<TapexS2Hit>& L_s2_hits
+    const ROOT::RVec<S2Hit>& R_s2_hits,
+    const ROOT::RVec<S2Hit>& L_s2_hits
 );
 
-ROOT::RVec<TapexEventHandler> gen_coinc_window_events(
+ROOT::RVec<EventHandler> gen_coinc_window_events(
     double dt_min, 
     double dt_max, 
     double beam_current, 
     unsigned int run_number,
-    const ROOT::RVec<TapexS2Hit>& R_s2_hits,
-    const ROOT::RVec<TapexS2Hit>& L_s2_hits
+    const ROOT::RVec<S2Hit>& R_s2_hits,
+    const ROOT::RVec<S2Hit>& L_s2_hits
 );
 
-ROOT::RVec<ApexVDC::ChamberPair> gen_pairs( 
-    const TapexEventHandler& evt, 
-    ROOT::RVec<ApexVDC::HitGroup>& gVec_U, 
-    ROOT::RVec<ApexVDC::HitGroup>& gVec_V, 
+ROOT::RVec<VDC::ChamberPair> gen_pairs( 
+    const EventHandler& evt, 
+    ROOT::RVec<VDC::HitGroup>& gVec_U, 
+    ROOT::RVec<VDC::HitGroup>& gVec_V, 
     bool is_LoChamber 
 );
 
 
-void gen_pid_data(const bool is_RHRS, RDFNodeAccumulator& rna, const ApexPidManager* pid_manager);
+void gen_pid_data(const bool is_RHRS, RDFNodeAccumulator& rna, const PidManager* pid_manager);
 
-ROOT::RVec<ApexVDC::Track> gen_rawtracks( 
-    TapexEventHandler& evt, 
-    ROOT::RVec<ApexVDC::ChamberPair>& pairs_Lo, 
-    ROOT::RVec<ApexVDC::ChamberPair>& pairs_Hi 
+ROOT::RVec<VDC::Track> gen_rawtracks( 
+    EventHandler& evt, 
+    ROOT::RVec<VDC::ChamberPair>& pairs_Lo, 
+    ROOT::RVec<VDC::ChamberPair>& pairs_Hi 
 ); 
 
 /// @brief Generates react vertex
 /// @param rna RDFNodeAccumulator to add branches to 
-/// @param vtx const ptr to TapexReactVertex obj (arm specific!)
-void gen_react_vertex(RDFNodeAccumulator& rna, TapexReactVertex* vtx);
+/// @param vtx const ptr to ReactVertex obj (arm specific!)
+void gen_react_vertex(RDFNodeAccumulator& rna, ReactVertex* vtx);
 
-ROOT::VecOps::RVec<TapexS2Hit> generate_S2_hits(
+ROOT::VecOps::RVec<S2Hit> generate_S2_hits(
     const bool is_RHRS,
     const ROOT::RVec<double>& PMT_R, 
     const ROOT::RVec<double>& PMT_L 
@@ -97,14 +97,14 @@ void generate_vdc_tracks(
     EventCounter_RPtr &nPass_1refinedTrack
 );
 
-/// @brief Form 'ApexVDC::HitGroup's from groups of VDC hits
+/// @brief Form 'VDC::HitGroup's from groups of VDC hits
 /// @param evt Apex event handler
 /// @param p the VDC plane in question, [0,..,3].
 /// @param h_rawtime a collection of all VDC wire rawtimes 
 /// @param h_wire a collection of all VDC wire numbers
 /// @return a vector of all valid VDC hit groups 
-ROOT::RVec<ApexVDC::HitGroup> group_vdc_hits ( 
-    const TapexEventHandler& evt, 
+ROOT::RVec<VDC::HitGroup> group_vdc_hits ( 
+    const EventHandler& evt, 
     int   p, 
     const ROOT::RVec<double>& h_wire,
     const ROOT::RVec<double>& h_rawtime 
@@ -114,13 +114,13 @@ ROOT::RVec<ApexVDC::HitGroup> group_vdc_hits (
 // Refine tracks from the hi-chamber using newton's method
 //   
 void refine_track( 
-    ApexVDC::Track& trk, 
+    VDC::Track& trk, 
     const int nCycles=10, 
     double sigma=25e-9 
 ); 
 
-double Phi_model(const ApexVDC::Track& track);
-double Theta_model(const ApexVDC::Track& track);
+double Phi_model(const VDC::Track& track);
+double Theta_model(const VDC::Track& track);
 
 }
 }
