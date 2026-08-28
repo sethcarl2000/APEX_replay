9dea748a1fd09774a03aa59fdd143e9e53440d08
Seth Hall <sethhall@jlab.org>
2026-08-28 10:18:31 -0400

    Update theta_phi_model

--- replay/helpers/theta_phi_model.cxx
+++ replay/helpers/theta_phi_model.cxx
@@ -1,8 +1,8 @@
 
-#include "APEX_replay_helpers.h"
-#include <ApexVDCTrack.h> 
+#include <APEX/replay/helpers.h>
+#include <APEX/VDC/Track.h> 
 #include <math.h> 
-#include <ApexUtils.h> 
+#include <APEX/Utils.h> 
 
 
 namespace APEX
@@ -12,14 +12,14 @@ namespace replay
 namespace helpers
 {
 
-double Phi_model(const ApexVDC::Track& track) { 
+double Phi_model(const VDC::Track& track) { 
     
     return 
     -0.231*APEX::square(track.S2_y()) 
     +0.2532*track.S2_y() 
     +0.539e-3; 
 };
-double  Theta_model(const ApexVDC::Track& track) { 
+double  Theta_model(const VDC::Track& track) { 
     
     return 0.109648*track.S2_x(); 
 };
