37e763160d794225bf248fdb99c97299a5cd684d
sethcarl2000 <sethhall@jlab.org>
2026-08-30 19:34:07 -0400

    Add 'get_file_size_KB', and have PathCheckStatus be passed as optional paramter, as an alternative to throwing exceptions

--- utils/include/APEX/utils.h
+++ utils/include/APEX/utils.h
@@ -27,7 +27,7 @@ std::optional<int> get_env_variable_int(const std::string& param);
 
 /// @return the number of available cpus (wether in a slurm job or not)
 unsigned int get_available_cpus(); 
-
+  
 /// @return a 'FileCheck' 
 struct PathCheckStatus { 
 
@@ -42,10 +42,19 @@ PathCheckStatus is_path_regular_file(const std::string& path);
 /// @brief Check if the given path 1.) exists and 2.) is a directory 
 PathCheckStatus is_path_directory(const std::string& path); 
 
-/// @brief Remove a file from disk. throws exception if it doesn't succeed
+/// @brief Remove a file from disk. throws exception if it doesn't succeed (unless non-null status is passed, then error message is given to 'status' and no expcetion is thrown). 
 /// @param path path to file to be removed
-void remove_file_from_disk(const std::string& path);
+  void remove_file_from_disk(const std::string& path, PathCheckStatus* status=nullptr);
 
+/// @brief Based on ifarm's slurm configuration
+/// @return path to the root directory of this slurm job's /scratch disk allocation. if it detects we are **not** in a slurm job / array, or the directory is missing, then throws a std::runtime_exception. 
+std::string get_slurm_scratch_directory(PathCheckStatus* status=nullptr);
+
+/// @brief get the size of the file, in KB.
+/// @param path path to file
+/// @param status status object. if the status object is not null, and the size check fails, then the status will be populated with the error message. If the check fails, and the status is null, then an exception is thrown 
+double get_file_size_KB(const std::string& path, PathCheckStatus* status=nullptr);
+  
 /// @brief Attempts to fit a gaussian to a histogram, with a constant background
 /// @param hist histogram to fit
 /// @param radius radius of fit
@@ -60,9 +69,6 @@ void fit_gaus_to_hist(
     bool do_draw=false
 ); 
 
-/// @brief Based on ifarm's slurm configuration
-/// @return path to the root directory of this slurm job's /scratch disk allocation. if it detects we are **not** in a slurm job / array, or the directory is missing, then throws a std::runtime_exception. 
-std::string get_slurm_scratch_directory();
 
 /// @brief Raise value 'x' to integer power 'N'
 /// @tparam N power. only valid for N = 2,3,4.  
