d28f75c78cde47b4e14477a082ca78b222ca0ce2
Seth Hall <sethhall@jlab.org>
2026-08-31 01:00:07 -0400

    Add new version that deploys tarball

--- scripts/run_full_replay_chunks.sh
+++ scripts/run_full_replay_chunks.sh
@@ -10,88 +10,35 @@ min_raw_data_MB=1.0
 run0=${1}
 run1=${2}
 
-run=$(( ${run0} - 1 ))
+# max number of CODA events to shoot for. If adding one more run overflows this value, then skip it. 
+max_events_per_task=7500000
 
-# maximum-ish events to process
-raw_event_cap=2500000
+# this gives us the number of CODA events in each raw file 
+path_coda_events_CSV="logs/n-CODA-events-per-file.csv" 
 
-path_rawfile_data="logs/rawfile-sizes.list"
+# this file will tell each array which jobs it must complete. 
+array_file="${PATH_APEX_REPLAY}/array-tasks.csv"
 
-array_file_stem="${PATH_APEX_VOLATILE}/slurm/array_lists/array_${run0}_${run1}"
+# make an array file for the csv 
+root -l -b -q "macros/create_task_csv.C(\"${path_coda_events_CSV}\", \"${array_file}\", ${run0}, ${run1}, ${max_events_per_task})" 
 
-array_events=0
-array_index=0
-array_file="${array_file_stem}_${array_index}.list" 
 
-if [[ -f "${array_file}" ]]; then rm "${array_file}"; fi 
-touch "${array_file}"
+path_tarball="${PATH_APEX_REPLAY}/slurm_payloads/payload__$(date +%Y.%m.%d_%H.%M)__run-${run0}_run-${run1}.tar.gz" 
 
-min_events_per_file=750
+tar -czf "${path_tarball}" build utils decode replay array-tasks.csv execute_array_task.C 
 
-while [[ ${run} -lt ${run1} ]]
+# find the last array in the csv 
+last_array_id=0
+while read -r line 
 do
+	IFS=' ' read -ra line_array <<< "${line}"
+	last_array_id="${line_array[0]}"
 
-    run=$(( $run + 1 ))
-    
-    while read -r line;
-    do
-	# skip the line if its empty 
-	if [[ -z "${line}" ]]; then continue; fi 
-	
-	# now, split the line into an array
-	IFS='|' read -ra line_array <<< "${line}"
-	
-	line_run="${line_array[0]}"
-	
-	if [[ "${line_run}" -lt "${run}" ]]; then continue; fi
-	
-	if [[ "${line_run}" -gt "${run}" ]]; then
-	    run=$(( $run + 1 ))
-	    if [[ "${run}" -gt "${run1}" ]]; then break; fi 
-	fi
-	
-	rawfile_num="${line_array[1]}"
-	start_event="${line_array[2]}"
-	end_event="${line_array[3]}"
-	events_in_file="${line_array[4]}"
-	path="${line_array[5]}"
-	
-	if [[ $events_in_file -lt $min_events_per_file ]]
-	then
-	    continue;
-	fi
-	
-	# add this file to the list of files to process
-	echo "${line_run}|${rawfile_num}|${start_event}|${end_event}|${path}" >> "${array_file}" 
+done < <(cat ${array_file})
 
-	array_events=$(( ${array_events} + ${events_in_file} ))
-	if [[ ${array_events} -ge ${raw_event_cap} ]]
-	then
-	    echo "<${0}>: array task ${array_index} assigned ${array_events} raw events."
-	    array_events=0
-	    array_index=$(( $array_index + 1 ))
-	    array_file="${array_file_stem}_${array_index}.list"
-	    if [[ -f "${array_file}" ]]; then rm "${array_file}"; fi 
-	    touch "${array_file}" 
-	fi
-        
-    done < <(cat "${path_rawfile_data}")
-    
-done 
+echo "last array id: ${last_array_id}"
 
-#now, let's submit the jobs.
-
-#check if the last array file is empty
-n_rows_last=$(cat ${array_file} | wc -l) 
-if [[ $n_rows_last -lt 1 ]]
-then
-    array_last=$(( $array_index - 1 ))
-else
-    array_last=$array_index
-fi
-
-
-cmd_string="sbatch --job-name=apex_replay_${run0}_${run1} --array=0-${array_last} scripts/run-full-replay-array ${array_file_stem}" 
+cmd_string="sbatch --job-name=apex_replay_${run0}_${run1} --array=0-${last_array_id} scripts/run-full-replay-array ${array_file}" 
 echo "${cmd_string}" 
 
 eval ${cmd_string}
