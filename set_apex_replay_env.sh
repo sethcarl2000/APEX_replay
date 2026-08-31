4235d813f5e92d6835900fbb7acf2a307734266d
Seth Hall <sethhall@jlab.org>
2026-08-30 22:49:55 -0400

    Have all shared libraries put their '.so' files in one place

--- set_apex_replay_env.sh
+++ set_apex_replay_env.sh
@@ -42,8 +42,7 @@ export ROOT_INCLUDE_PATH=$(add_path_to_list "${PATH_APEX_REPLAY}/replay/include"
 export ROOT_INCLUDE_PATH=$(add_path_to_list "${PATH_APEX_REPLAY}/utils/include" "${ROOT_INCLUDE_PATH}")
 
 # now, add the build directory to the library, so it can find the shared libraries 
-export LD_LIBRARY_PATH=$(add_path_to_list "${PATH_APEX_REPLAY}/build" "${LD_LIBRARY_PATH}")
-
+export LD_LIBRARY_PATH=$(add_path_to_list "${PATH_APEX_REPLAY}/build/lib" "${LD_LIBRARY_PATH}")
 
 
 
