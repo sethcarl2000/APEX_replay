f12048dc967f55acfdb64b53dd12bfd4be6af242
sethcarl2000 <sethhall@jlab.org>
2026-09-09 23:28:12 -0400

    Include the correct macro & the scripts directory in the tarball

--- scripts/run_full_replay_chunks.sh
+++ scripts/run_full_replay_chunks.sh
@@ -32,7 +32,7 @@ slurm_timestamp="$(date +'%Y-%m-%dT%H:%M:%S')"
 
 path_tarball="${PATH_APEX_REPLAY}/slurm_payloads/payload__${timestamp}__run-range_${run0}-${run1}.tar.gz" 
 
-tar -czf "${path_tarball}" build utils decode scripts replay DB outDefs array-tasks.csv execute_array_task.C set_apex_replay_env.sh
+tar -czf "${path_tarball}" build utils decode scripts replay DB outDefs array-tasks.csv execute_specific_run.C set_apex_replay_env.sh
 
 # find the last array in the csv 
 last_array_id=0
